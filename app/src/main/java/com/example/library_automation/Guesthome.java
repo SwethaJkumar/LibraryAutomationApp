3bcb976201159a4562e0438fc227f0c594b8d83a
SwethaJkumar <swethajkumar@gmail.com>
2022-03-26 13:36:35 +0530

    Initial Commit

--- app/src/main/java/com/example/library_automation/Guesthome.java
+++ app/src/main/java/com/example/library_automation/Guesthome.java
@@ -1,7 +1,10 @@
 package com.example.library_automation;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Guesthome extends AppCompatActivity {
@@ -10,5 +13,21 @@ public class Guesthome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guesthome);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
